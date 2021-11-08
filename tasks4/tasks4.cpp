8da50ae37bec882da82d533de52da6d81b21ec00
konovashka <anton_konovalov_1@mail.ru>
2021-11-08 23:27:25 +0300

    Добавьте файлы проекта.

---
+++ tasks4/tasks4.cpp
@@ -0,0 +1,30 @@
+﻿// является ли максимальный элемент последовательности чётным
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,x,max;
+    max = INT_MIN;
+    printf("vvedite dlinnu posledovatel'nosti\n");
+    scanf_s("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf_s("%d", &x);
+        if (max < x)
+        {
+            max = x;
+        }
+        printf("%d\n", max);
+    }
+    if (max%2==0)
+    {
+        printf("yavlyaetsa");
+    }
+    else
+    {
+        printf("ne yavlyaetsa");
+    }
+    return 0;
+}
+
